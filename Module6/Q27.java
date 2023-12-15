4a4102c53b67fb084762ea25199c3b6515bdd8e3
RISHI1299 <100058541+RISHI1299@users.noreply.github.com>
2023-12-15 10:41:11 +0530

    module6

---
+++ Module6/Q27.java
@@ -0,0 +1,65 @@
+package Module6;
+/*
+  W.A.J.P. to create a custom exception if Customer withdraw amount
+which is greater than account balance then program will show custom 
+exception otherwise amount will deduct from account balance.
+Account balance is:2000 Enter withdraw
+amount:2500
+Sorry, insufficient balance, you need more 500 Rs.To perform
+this transaction.
+
+*/
+import java.util.Scanner;
+
+class RishiException extends Exception
+{
+	double amount;
+	public RishiException(double amount)
+	{
+		this.amount=amount;
+	}
+}
+class ATM
+{
+	double balance;
+	public void deposit(double amount)
+	{
+		balance += amount;
+		System.out.println(amount+"rs deposite...");
+	}
+	public void withdrawal(double amount)throws RishiException
+	{
+		if(amount<=balance)
+		{
+			balance -= amount;
+			System.out.println(amount+"rs withdrawal sucessfully..");
+		}
+		else
+		{
+			double needs=amount - balance;
+			throw new RishiException(needs);
+		}
+	}
+}
+
+public class Q27 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		ATM a1=new ATM();
+		System.out.println("Enter the amount of deposit..");
+		double amount=sc.nextDouble();
+		a1.deposit(amount);
+		
+		System.out.println("Enter amount for withdeawal..");
+		amount=sc.nextDouble();
+		try
+		{
+			a1.withdrawal(amount);
+		}catch(RishiException e)
+		{
+			System.out.println("If your enter"+e.amount+"rs more into your account then you withdrawl"+amount+"rs");
+		}
+	}
+}
---
+++ Module6/Q9.java
@@ -0,0 +1,27 @@
+package Module6;
+
+//WAP to sum values of an array.
+
+import java.util.Scanner;
+
+public class Q9 
+{
+	public static void main(String[] args) 
+	{
+		int sum=0;
+		int[] a=new int[5];
+		Scanner sc=new Scanner(System.in);
+		for (int i=0; i<5; i++) 
+		{
+			System.out.println("Enter the Value..");
+			a[i]=sc.nextInt();
+		}
+		for (int i=0; i<5; i++) 
+		{
+			System.out.print(a[i]+" ");
+			sum=sum+a[i];
+		}
+		System.out.println();
+		System.out.println(sum);
+	}
+}
